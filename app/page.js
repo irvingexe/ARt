0f9feaf43cd521a881b89c4d87f30ffe9338874a
Irving Mariscales <mariscales.irving@gmail.com>
2023-11-02 21:24:30 -0700

    page.js

--- app/page.js
+++ app/page.js
@@ -1,11 +1,9 @@
 import React from 'react'
 import Qr from './components/Qr'
-import Script from 'next/script'
 
 export default function page() {
   return (
     <>
-      <Script src="https://launchar.app/sdk/v1?key=XkiKqjr09vKqXQGgNE0VnQBhodJWlhD6&redirect=true" strategy='beforeInteractive'></Script>
       <div><Qr/></div>
     </>
   )
