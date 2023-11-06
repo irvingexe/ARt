cd5a563500521fea4d197489a6fc2d6d827d2bc4
Irving Mariscales <mariscales.irving@gmail.com>
2023-11-06 01:05:08 -0700

    more models

--- app/model/ModelContainer/StaticModel.js
+++ app/model/ModelContainer/StaticModel.js
@@ -4,8 +4,8 @@ import React from 'react'
 import { useLoader } from '@react-three/fiber'
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader'
 
-export default function StaticModel({position, model3d}) {
-  const gltf = useLoader(GLTFLoader, model3d)
+export default function StaticModel({position, model}) {
+  const gltf = useLoader(GLTFLoader, model)
 
   return <primitive object={gltf.scene} position={position} />
 }
--- app/model/ModelContainer/index.js
+++ app/model/ModelContainer/index.js
@@ -3,18 +3,29 @@ import Model from './StaticModel'
 import { PreviewWrap } from '../../components/PreviewWrap';
 import AnimatedModel from './AnimatedModel';
 import { useXrHits } from '../../contexts/XrHitProvider';
+import { useGLTF } from '@react-three/drei';
 
 export const ModelContainer = () => {
-
+  const modelURL = '/assets/models/druid.gltf';
+  const { nodes, materials, animations, parser } = useGLTF(modelURL);
   let hitPoints = useXrHits();
+  let model;
+
+  Array.from(parser.associations.keys()).forEach(e => {
+    if (e.type === 'SkinnedMesh') model = e.name
+  })
   
   return (
-    <PreviewWrap preview={<Model position={[0,0,0]} model3d={'/assets/models/druid.gltf'}/>}>
-      {/*<Model position={[...hitPoints].pop().position} model={'/assets/models/witch.gltf'}/>*/}
-      <AnimatedModel 
-        position={[...hitPoints].pop().position} 
-        model3d={'/assets/models/druid.gltf'}
-      />
+    <PreviewWrap preview={<Model position={[0,0,0]} model={modelURL}/>}>
+      {!animations.length 
+        ? <Model position={[...hitPoints].pop().position} model={modelURL}/>
+        : <AnimatedModel 
+            position={[...hitPoints].pop().position} 
+            model={model}
+            nodes={nodes}
+            materials={materials}
+            animations={animations}
+          />}
     </PreviewWrap>
   )
 }
